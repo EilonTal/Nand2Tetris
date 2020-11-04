56b8162136a1ffdbfa953a9c8ade34b2036fa57a
Eilon Tal <eilontal11@gmail.com>
2020-11-05 00:49:00 +0200

    test

--- 07/My VM Translator/Command_Handler.cpp
+++ 07/My VM Translator/Command_Handler.cpp
@@ -1,9 +1,9 @@
 //
 // Created by Roe on 8/22/2020.
 //
-
+//s
 #include "Command_Handler.h"
-
+//ok
 Command_Handler::Command_Handler(string input_path_str, string output_path_str):
  current_command(), input_file(&input_file), output_file(&output_file)
 {
